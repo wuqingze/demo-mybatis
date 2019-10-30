c84fd717621cff973ca77925ac554994a41bc30e
wuqingze <nongxiaolang@foxmail.com>
2019-10-30 10:45:49 +0800

    初始化项目

---
+++ src/main/java/com/example/demo/mapper/PeopleMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.People;
+
+public interface PeopleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(People record);
+
+    int insertSelective(People record);
+
+    People selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(People record);
+
+    int updateByPrimaryKey(People record);
+}
